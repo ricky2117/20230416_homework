5c808b3972f6dd22b760c39a066e6a8832aa9133
ricky2117 <129981514+ricky2117@users.noreply.github.com>
2023-04-17 00:15:46 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		//題目from leetcode 題庫第一題，指定陣列，並指定陣列2位數總和，打印出此2位數。
+		static void Main(string[] args)
+		{
+			Console.WriteLine("請輸入任一陣列(請用\",\"分隔):");
+			string input_list = Console.ReadLine();
+			Console.WriteLine("請輸入陣列中某2位相加的總和:");
+			int input_target = int.Parse(Console.ReadLine());
+			List_target.List_two(input_list,input_target);
+
+			Console.ReadLine();
+		}
+	}
+	public static class List_target
+	{
+		public static void List_two(string list, int target)
+		{
+			bool flag = false;
+			List<int> arr = list.Split(',').Select(int.Parse).ToList();
+            for (int i = 0; i < arr.Count; i++)
+			{
+				for (int k = i+1; k < arr.Count; k++)
+				{
+					if (arr[i] + arr[k] == target)
+					{
+                        Console.WriteLine($"[{i},{k}]");
+						flag = true;
+                    }
+				}	
+			}
+			if (!flag) { Console.WriteLine("陣列中無符合的數字"); }
+		}
+
+	}
+
+
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Person> list = new List<Person>()
+			{ new Employee{Name ="A"},
+			 new Employee {Name ="B",Salary = 100} };
+
+			foreach (Person person in list)
+			{
+
+				//Console.WriteLine(person.GetMessage());
+				person.GetMessage();
+			}
+			Console.ReadLine();
+		}
+	}
+	public abstract class Person
+	{
+		public string Name { get; set; }
+		public abstract void GetMessage();
+		//{
+		//	return $"{Name}";
+		//}
+    }
+	public class Employee : Person
+	{
+		public int Salary { get; set; }
+		public override void GetMessage()
+		{
+			 throw new NotFiniteNumberException();
+			//return $"{Name},{Salary}";
+		}
+	}
+
+}
